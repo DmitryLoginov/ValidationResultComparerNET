1fd695fae57f9eee475dadb2f75bc7f21baefa3c
Dmitry Loginov <daimom25@gmail.com>
2024-10-20 20:31:19 +0300

    Created class for csv

---
+++ Model/Csv.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Model;
+
+public class Csv
+{
+    public string[] Header {get; set;} = [];
+
+    public string[][] Data {get; set;} = [];
+
+    public int[] Key {get; set;} = [];
+}
---
+++ Utils/CsvCreator.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text.Json;
+
+using Model;
+
+namespace Utils;
+
+public static class CsvCreator
+{
+    public static void CreateCheckResultCsv(int records)
+    {
+        if (records <= 0)
+        {
+            return;
+        }
+
+        Csv csv= new Csv();
+        
+        for (int i = 0; i < records; i++)
+        {
+
+        }
+    }
+}
