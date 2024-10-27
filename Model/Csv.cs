94fe6bd0c43d4cf3c96d48ad77024dd304f9e487
Dmitry Loginov <daimom25@gmail.com>
2024-10-27 21:00:48 +0300

    Added constructor for csv

--- Model/Csv.cs
+++ Model/Csv.cs
@@ -4,9 +4,71 @@ namespace Model;
 
 public class Csv
 {
-    public string[] Header {get; set;} = [];
+    //private readonly string _lineBreak;
+    
+    public string[] Header {get; private set;}
 
-    public string[][] Data {get; set;} = [];
+    public List<List<string>> Data {get; private set;} = new List<List<string>>();
 
-    public int[] Key {get; set;} = [];
+    public int[] Key {get; private set;}
+
+    public Csv(string content, bool firstRowHeader = true)
+    {
+        //TODO: line break
+        var rows = content.Split('\n');
+
+        //TODO: empty line
+        if (firstRowHeader)
+        {
+            Header = rows[0].Split(';');
+            //TODO: ranges
+            var dataRowsCount = rows.Count() - 1;
+            for (int i = 1; i < rows.Count(); i++)
+            {
+                Data.Add(rows[i].Split(';').ToList());
+            }
+        }
+        else
+        {
+            //TODO: linq
+            string headerString = string.Empty;
+            var count = rows[0].Split(';').Count();
+            for (int i = 0; i < count; i++)
+            {
+                headerString += $"column{i + 1}";
+                if (i != count - 1)
+                {
+                    headerString += ";";
+                }
+            }
+            Header = headerString.Split(';');
+            var dataRowsCount = rows.Count() - 1;
+            for (int i = 0; i < rows.Count(); i++)
+            {
+                Data.Add(rows[i].Split(';').ToList());
+            }
+        }
+    }
+
+    //TODO: validation
+    public void SetKeys(params int[] keys)
+    {
+        Key = keys;
+    }
+
+    /*private void SetLineBreak()
+    {
+        var os = System.Environment.OSVersion.Platform;
+        switch (os)
+        {
+            case PlatformID.Win32NT:
+            case PlatformID.Win32S:
+            case PlatformID.Win32Windows:
+            case PlatformID.WinCE:
+            {
+                _lineBreak = "\n\r";
+                break;
+            }
+        }
+    }*/
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Model;
+using System.IO;
+using System.Text;
+
+string line;
+StringBuilder sb = new StringBuilder();
+try
+{
+    StreamReader sr = new StreamReader("/home/dmitry/Загрузки/testCsvWithoutHeader.csv");
+    line = sr.ReadLine();
+    while(line != null)
+    {
+        sb.AppendLine(line);
+        line = sr.ReadLine();
+    }
+    sr.Close();
+
+    //System.Console.WriteLine(sb.ToString());
+
+    Csv csv = new Csv(sb.ToString(), false);
+
+    System.Console.WriteLine(string.Join(";", csv.Header));
+    System.Console.WriteLine(string.Join(";", csv.Data[0]));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.ReadLine();
+}
+
+
+Console.ReadLine();
